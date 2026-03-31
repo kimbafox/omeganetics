afedb184a59ff1955a74f506d1d6ed27d968746e
kimbafox <juegocrisger@gmail.com>
2026-03-30 22:24:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// servir archivos estáticos
+app.use(express.static(__dirname));
+
+// abrir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
+});
